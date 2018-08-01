9c82f912e84f43832617eb342f3bb8587f8504e5
Jørgen Ellingsen <jorgen@ellera.no>
2018-08-01 13:05:32 +0200

    Added exceptions when PB information is not accessible

--- porterbuddy-wc/includes/display_filters.php
+++ porterbuddy-wc/includes/display_filters.php
@@ -12,11 +12,11 @@ function pb_before_checkout_create_order( $order, $data ) {
 		{
 			if($item->get_method_id() == PORTERBUDDY_PLUGIN_NAME)
 			{
-				// Nothing?
+				if(WC()->session->get('pb_windowStart') == NULL || WC()->session->get('pb_windowStart')) throw new Exception('PorterBuddy delivery window must be set');
+				elseif(lookup_window(WC()->session->get('pb_windowStart')) == null) throw new Exception('Invalid delivery window for PorterBuddy');
 			}
 		}
 	}
-	$order->save();
 }
 add_action('woocommerce_checkout_create_order', 'pb_before_checkout_create_order', 20, 2);
 
@@ -77,15 +77,7 @@ function pb_display_order_complete( $order_id ) {
 
 						if(isset($window_start) && strlen($window_start) > 6)
 						{
-							$window = null;
-							$api_result = include dirname(__FILE__).'/availability.php';
-							if(isset($api_result['data'][$type]))
-							{
-								foreach ($api_result['data'][$type] as $win) {
-									if ($win->start == $window_start) $window = Window::load($win);
-									break;
-								}
-							}
+							$window = lookup_window($window_start);
 
 							if($window != null) {
 								if ( $return_on_demand ) {
@@ -219,6 +211,19 @@ function render_delivery_message($start, $end)
 	$start_dto = new DateTime($start);
 	$end_dto = new DateTime($end);
 	return sprintf( __( 'The order will be delivered between %s and %s on %s'), $start_dto->format('H:i'), $end_dto->format('H:i'), $start_dto->format('l jS F'));
+}
+
+function lookup_window()
+{
+	$window = null;
+	$api_result = include dirname(__FILE__).'/availability.php';
+	if(isset($api_result['data'][$type]))
+	{
+		foreach ($api_result['data'][$type] as $win) {
+			if ($win->start == $window_start) $window = Window::load($win);
+			break;
+		}
+	}
 }
 add_action( 'woocommerce_admin_order_data_after_shipping_address', 'pb_admin_display', 10, 1 );
 
